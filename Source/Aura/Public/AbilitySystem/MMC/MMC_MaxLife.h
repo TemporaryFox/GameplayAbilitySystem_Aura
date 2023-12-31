180b4791e4597f9b99f095b15a19dd63e4a4ac81
TemporaryFox <robert.kernohan@gmail.com>
2023-12-31 23:11:29 +1030

    Lesson 74

---
+++ Source/Aura/Public/AbilitySystem/MMC/MMC_MaxLife.h
@@ -0,0 +1,25 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_MaxLife.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UMMC_MaxLife : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+public:
+	UMMC_MaxLife();
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+
+private:
+
+	FGameplayEffectAttributeCaptureDefinition VigorDef;
+	
+};
