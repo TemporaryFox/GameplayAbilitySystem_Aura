5364aa38c0d40dbce5cc7d374b9785baec8e6f68
Robert Kernohan <robert.kernohan@gmail.com>
2024-02-02 23:17:55 +1030

    Lesson 148

---
+++ Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalcDamage.h
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalcDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UExecCalcDamage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+public:
+	UExecCalcDamage();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
